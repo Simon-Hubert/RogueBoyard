2f12586dc261688054799d440a555b6c8b09d258
Enzo Bossé <117042107+Bl0bli@users.noreply.github.com>
2025-01-07 19:18:45 +0100

    j'aipascommit

---
+++ Source/RogueBoyard/Private/Camera/CameraTrailerWorldSubsystem.cpp
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Camera/CameraTrailerWorldSubsystem.h"
+
+#include "ImageUtils.h"
+#include "Components/SceneCaptureComponent2D.h"
+
+#include  "Engine/TextureRenderTarget2D.h"
+#include "Exporters/TextureExporterPNG.h"
+#include "Serialization/BufferArchive.h"
+
+void UCameraTrailerWorldSubsystem::AddCamera(USceneCaptureComponent2D* Camera)
+{
+	if(RenderTargetTextureMap.FindKey(Camera)) return;
+	
+	for (UTextureRenderTarget2D* Element : RenderTargets)
+	{
+		if(!RenderTargetTextureMap.Contains(Element))
+		{
+			RenderTargetTextureMap.Add(Element, Camera);
+			Camera->TextureTarget = Element;
+			break;
+		}
+	}
+}
+
+void UCameraTrailerWorldSubsystem::PostInitialize()
+{
+	Super::PostInitialize();
+	const UTrailerRenderingSettings* Settings = GetDefault<UTrailerRenderingSettings>();
+	for (int i = 0; i < Settings->RenderTargets.Num(); i++)
+	{
+		UTextureRenderTarget2D* RenderTarget = Settings->RenderTargets[i].LoadSynchronous();
+		RenderTargets.Add(RenderTarget);
+	}
+}
+
+void UCameraTrailerWorldSubsystem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	Frame++;
+	for (auto Element : RenderTargetTextureMap)
+	{
+		FString ThumbnailFile = "C:/Users/Enzo/Desktop/RogueBoyard/Trailer/" + Element.Value->GetOwner()->GetName() + "/" + FString::FromInt(Frame) + ".png";
+		FArchive* Ar = IFileManager::Get().CreateFileWriter(*ThumbnailFile);
+		FBufferArchive Buffer;
+		
+		bool bSuccess = FImageUtils::ExportRenderTarget2DAsPNG(Element.Key, Buffer);
+		if (bSuccess)
+		{
+			Ar->Serialize(const_cast<uint8*>(Buffer.GetData()), Buffer.Num());
+		}
+
+		delete Ar;
+	}
+}
---
+++ Source/RogueBoyard/Public/Camera/CameraTrailerWorldSubsystem.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TrailerRenderingSettings.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "CameraTrailerWorldSubsystem.generated.h"
+/**
+ * 
+ */
+UCLASS()
+class ROGUEBOYARD_API UCameraTrailerWorldSubsystem : public UTickableWorldSubsystem
+{
+	GENERATED_BODY()
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void AddCamera(USceneCaptureComponent2D* Camera);
+	virtual void PostInitialize() override;
+	virtual void Tick(float DeltaTime) override;
+	virtual TStatId GetStatId() const override {return TStatId(); };
+private:
+	TArray<UTextureRenderTarget2D*> RenderTargets;
+	TMap<UTextureRenderTarget2D*, USceneCaptureComponent2D*> RenderTargetTextureMap;
+
+	int Frame = 0;
+};
